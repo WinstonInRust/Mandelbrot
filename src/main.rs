2b3957960e2dde45cc73d04a1279ece3cadb8cc3
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-26 10:38:46 +0200

    added the complex numbers crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use num::Complex;
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+        println!("{:?}", z);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    complex_square_add_loop(2.0.into());
 }
