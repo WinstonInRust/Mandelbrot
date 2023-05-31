c41b950ffd9809d983995c41c1266ee807e7f6fb
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-31 09:56:44 +0200

    Added CLI function for parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -10,6 +11,27 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
     None
 }
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
 fn main() {
     escape_time(Complex { re: 0.0, im: 0.0 }, 1000);
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
