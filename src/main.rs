49476df946b3da0478b658e16aa19c9c9a5c2307
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-31 10:39:25 +0200

    Testing functions

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 }
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
-    match parse_pair::<f64>(s, ',') {
+    match parse_pair(s, ',') {
         None => None,
         Some((re, im)) => Some(Complex { re, im }),
     }
@@ -41,4 +41,16 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
